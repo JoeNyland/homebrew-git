19dd031f2ce696c36d9a5d23fae6cad78c5b607d
Joe Nyland <joenyland@me.com>
2017-03-12 15:58:31 +0000

    Typo

--- Formula/git-vommit.rb
+++ Formula/git-vommit.rb
@@ -1,5 +1,5 @@
 class GitVommit < Formula
-  desc "How many times have you typed `git commit` instead of `git vommit`?"
+  desc "How many times have you typed `git vommit` instead of `git commit`?"
   homepage "https://github.com/JoeNyland/git-vommit"
   url "https://github.com/JoeNyland/git-vommit/archive/1.0.0.tar.gz"
   sha256 "0fe50850c9d5522f33bd00057b15fc40e0efdf85fa90f9d0bbfede0ff18e29dd"
